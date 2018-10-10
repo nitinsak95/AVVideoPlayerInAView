df2ebac130fd9f6dd9e14da902170a4221e52ef3
affixusimac1 <viveking@outlook.com>
2018-10-10 12:34:56 +0530

    commit

--- videoSample1/ViewController.swift
+++ videoSample1/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    var videoPlayer = AVPlayer()
+    var avPlayerViewController = AVPlayerViewController()
+    
+    @IBOutlet weak var vwMain: UIView!
+    @IBOutlet weak var avPlayerView: UIView!
+    var tapGesture = UITapGestureRecognizer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+//
+//        self.playVideo(url: videoURL!)
+        
+//        guard let movieURL = URL(string: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
+//            else { return }
+        let videoURL = URL(string: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
+        videoPlayer = AVPlayer(url: videoURL!)
+        avPlayerViewController.player = videoPlayer
+        avPlayerViewController.view.frame = avPlayerView.bounds
+        self.addChildViewController(avPlayerViewController)
+        avPlayerView.addSubview(avPlayerViewController.view)
+        avPlayerViewController.didMove(toParentViewController: self)
+        videoPlayer.play()
+        
     }
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        let videoURL = URL(string: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
+//
+//        self.playVideo(url: videoURL!)
+//    }
+    
+    func playVideo(url: URL) {
+        self.videoPlayer = AVPlayer(url: url)
+        self.avPlayerViewController = AVPlayerViewController()
+        self.avPlayerViewController.player = self.videoPlayer
+        avPlayerViewController.view.frame = avPlayerView.frame
+        
+        avPlayerViewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.addChildViewController(avPlayerViewController)
+        self.view.addSubview((avPlayerViewController.view)!)
+        videoPlayer.play()
+        
     }
-
 
 }
 
